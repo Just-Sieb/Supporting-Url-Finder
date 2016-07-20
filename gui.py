535d27d43299ee4057ac7cc0edcb237194d109ec
Justin Siebert <just.sieb@gmail.com>
2016-07-20 11:35:25 -0400

    added in scrollbar

--- gui.py
+++ gui.py
@@ -4,18 +4,28 @@ from tkinter import ttk
 class MainWindow(ttk.Frame):
     def __init__(self, master):
         self.master = master 
+        self.master.title(string="Supporting URL Finder")
+        self.master.minsize(width=300, height=300)
+        self.master.maxsize(width=300, height=600)
+
         self.frame = ttk.Frame(self.master)        
         self.entry = ttk.Entry(self.frame)
         self.enter = ttk.Button(self.frame, text="Find Urls")
-        self.list = Listbox(self.frame)
-        self.entry.grid(row=0, column=0)
-        self.enter.grid(row=0,column=1)
-        self.list.grid(row=1, columnspan=2)
+        self.list = Listbox(self.frame, height=15)
+        self.entry.grid(row=0, column=0, sticky=W, padx=5, pady=5)
+        self.enter.grid(row=0, column=1, sticky=E, padx=5, pady=5)
+        self.list.grid(row=1, columnspan=2, sticky=W+E+N+S, padx=5, pady=5)
+
+        self.scrollbar = ttk.Scrollbar(self.frame)
+        self.scrollbar.grid(column=2, row=1, sticky=N+S)
+        self.list.config(yscrollcommand=self.scrollbar.set)
+        self.scrollbar.config(command=self.list.yview)
+
         self.frame.pack()
         self.populate_list()
 
     def populate_list(self):
-        for num in range(10):
+        for num in range(30):
             self.list.insert(END, num)
 
 if __name__ == '__main__':
